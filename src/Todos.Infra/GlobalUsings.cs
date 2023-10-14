85506c938699bb071821c5e627cca390f8b759c0
Patrick Sachmann <patrick.sachmann@live.de>
2023-10-14 17:49:42 +0200

    Introducing centrally managed dependencies

--- src/Todos.Infra/GlobalUsings.cs
+++ src/Todos.Infra/GlobalUsings.cs
@@ -3,7 +3,7 @@ global using FluentValidation;
 global using MediatR;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Configuration.Json;
+// global using Microsoft.Extensions.Configuration.Json;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
